c94b0353dedc534029cb34994115da15b68d6eb3
= <=>
2023-12-27 23:05:58 +0530

    fixes.

--- ApplicationsTab.cs
+++ ApplicationsTab.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data;
+using System.Data.SQLite;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
@@ -14,6 +15,8 @@ namespace WinFormsActiveTango
         private DataTable dt;
         private DataGridView dataGridView1;
         private TabPage tabPage;
+        private ListBox listBox1;
+
 
         public ApplicationsTab(TabPage tabPage)
         {
@@ -37,8 +40,15 @@ namespace WinFormsActiveTango
 
             tabPage.Controls.Add(dataGridView1);
 
+            listBox1 = new ListBox
+            {
+                Location = new Point(520, 300),
+                Size = new Size(480, 100)
+            };
+            tabPage.Controls.Add(listBox1);
+
             System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
-            timer.Interval = 3000; // 1 minute
+            timer.Interval = 3000; // 3sec
             timer.Tick += Timer_Tick;
             timer.Start();
         }
@@ -49,23 +59,81 @@ namespace WinFormsActiveTango
 
             IntPtr foregroundWindow = GetForegroundWindow();
 
-            foreach (Process process in Process.GetProcesses())
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=applications.db;Version=3;"))
             {
-                if (!string.IsNullOrEmpty(process.MainWindowTitle))
+                conn.Open();
+
+                string sql = "CREATE TABLE IF NOT EXISTS Applications (ID INTEGER PRIMARY KEY AUTOINCREMENT, Application TEXT, InFocus INTEGER, Screen INTEGER, Timestamp DATETIME)";
+
+                using (SQLiteCommand command = new SQLiteCommand(sql, conn))
                 {
-                    DataRow row = dt.NewRow();
-                    row["Application"] = process.MainWindowTitle;
-                    row["In Focus"] = process.MainWindowHandle == foregroundWindow;
+                    command.ExecuteNonQuery();
+                }
 
-                    // Get the screen that the application's main window is located on
-                    Screen screen = Screen.FromHandle(process.MainWindowHandle);
-                    row["Screen"] = Array.IndexOf(Screen.AllScreens, screen) + 1;
+                foreach (Process process in Process.GetProcesses())
+                {
+                    if (!string.IsNullOrEmpty(process.MainWindowTitle))
+                    {
+                        DataRow row = dt.NewRow();
+                        row["Application"] = process.MainWindowTitle;
+                        row["In Focus"] = process.MainWindowHandle == foregroundWindow;
 
-                    dt.Rows.Add(row);
+                        // Get the screen that the application's main window is located on
+                        Screen screen = Screen.FromHandle(process.MainWindowHandle);
+                        row["Screen"] = Array.IndexOf(Screen.AllScreens, screen) + 1;
+
+                        dt.Rows.Add(row);
+
+                        sql = "INSERT INTO Applications (Application, InFocus, Screen, Timestamp) VALUES (@Application, @InFocus, @Screen, @Timestamp)";
+
+                        using (SQLiteCommand command = new SQLiteCommand(sql, conn))
+                        {
+                            command.Parameters.AddWithValue("@Application", process.MainWindowTitle);
+                            command.Parameters.AddWithValue("@InFocus", process.MainWindowHandle == foregroundWindow ? 1 : 0);
+                            command.Parameters.AddWithValue("@Screen", Array.IndexOf(Screen.AllScreens, screen) + 1);
+
+                            // Get the current time in the Asia/Kolkata timezone
+                            TimeZoneInfo timeZone = TimeZoneInfo.FindSystemTimeZoneById("Asia/Kolkata");
+                            DateTime currentTime = TimeZoneInfo.ConvertTime(DateTime.Now, timeZone);
+                            command.Parameters.AddWithValue("@Timestamp", currentTime);
+
+                            command.ExecuteNonQuery();
+                        }
+                    }
                 }
             }
 
             dataGridView1.DataSource = dt;
+
+            ShowTopApplications();
         }
+
+        private void ShowTopApplications()
+        {
+            listBox1.Items.Clear(); // Clear the ListBox
+
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=applications.db;Version=3;"))
+            {
+                conn.Open();
+
+                string sql = "SELECT Application, SUM(InFocus) as SecondsInFocus FROM Applications WHERE InFocus = 1 GROUP BY Application ORDER BY SecondsInFocus DESC LIMIT 3";
+
+                using (SQLiteCommand command = new SQLiteCommand(sql, conn))
+                {
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string application = reader["Application"].ToString();
+                            int secondsInFocus = Convert.ToInt32(reader["SecondsInFocus"]);
+
+                            // Display the application and count
+                            listBox1.Items.Add($"{application}: {secondsInFocus} seconds");
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
