e3116c5c26f9995e60ccb46905462d915ad2e1dc
= <=>
2023-12-31 13:43:09 +0530

    time bucket changes

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.Windows.Forms;
 using System.Data.SQLite;
 using System.Globalization;
 using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
 
 namespace WinFormsActiveTango
 {
@@ -33,6 +34,11 @@ namespace WinFormsActiveTango
         private ApplicationsTab applicationsTab;
         private Button unlockButton;
         private int MinutesUntilBlock_updated = 1;
+
+        private GroupBox timeBucketsGroupBox;
+        private ListBox timeBucketsListBox;
+        private Button useButton;
+        private Button loadTimeBucketButton;
 
         public Form1()
         {
@@ -77,12 +83,31 @@ namespace WinFormsActiveTango
             tabPage1.Controls.Add(updateGroup);
             tabPage1.Controls.Add(closeGroup);
 
+            // Add Time Buckets group box after closeGroup
+            timeBucketsGroupBox = new GroupBox { Text = "Time Buckets", Location = new Point(closeGroup.Left, closeGroup.Bottom + 10), Size = new Size(closeGroup.Width, 150) };
+            tabPage1.Controls.Add(timeBucketsGroupBox);
+
+            timeBucketsListBox = new ListBox { Location = new Point(10, 20), Size = new Size(timeBucketsGroupBox.Width - 20, 50) };
+            timeBucketsGroupBox.Controls.Add(timeBucketsListBox);
+            LoadTimeBuckets();
+
+            useButton = new Button { Text = "Use", Location = new Point(10, timeBucketsListBox.Bottom + 10), Size = new Size(timeBucketsGroupBox.Width - 20, 20) };
+            useButton.Click += UseButton_Click;
+            timeBucketsGroupBox.Controls.Add(useButton);
+
+            loadTimeBucketButton = new Button { Text = "Load Time Bucket", Location = new Point(10, useButton.Bottom + 10), Size = new Size(timeBucketsGroupBox.Width - 20, 20) };
+            loadTimeBucketButton.Click += LoadTimeBucketButton_Click;
+            timeBucketsGroupBox.Controls.Add(loadTimeBucketButton);
+
+
+
+
             tabPage2 = new TabPage("Applications");
 
             tabControl = new TabControl
             {
                 Location = new Point(10, 10),
-                Size = new Size(1000, 500),
+                Size = new Size(1000, 600),
                 ItemSize = new Size(100, 20) // Set the width and height of the tabs
             };
             tabControl.TabPages.Add(tabPage1);
@@ -109,6 +134,83 @@ namespace WinFormsActiveTango
 
            
         }
+
+        private void LoadTimeBuckets()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=tasks.db;Version=3;"))
+            {
+                conn.Open();
+
+                string sql = "SELECT * FROM TimeBuckets";
+
+                using (SQLiteCommand command = new SQLiteCommand(sql, conn))
+                {
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string bucketName = reader["BucketName"].ToString();
+                            string time = reader["Time"].ToString();
+                            timeBucketsListBox.Items.Add(bucketName + "(" + time + ")");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void LoadTimeBucketButton_Click(object sender, EventArgs e)
+        {
+            string header;
+
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=tasks.db;Version=3;"))
+            {
+                conn.Open();
+
+                // Delete all rows from the TimeBuckets table
+                string sql = "DELETE FROM TimeBuckets";
+
+                using (SQLiteCommand command = new SQLiteCommand(sql, conn))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                // Read the CSV file
+                using (StreamReader reader = new StreamReader("TimeBuckets.csv"))
+                {
+                    header = reader.ReadLine(); // Keep the header
+
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        string[] fields = line.Split(',');
+
+                        string bucketName = fields[0];
+                        string time = fields[1];
+
+                        // Insert the row into the TimeBuckets table
+                        sql = "INSERT INTO TimeBuckets (BucketName, Time) VALUES (@BucketName, @Time)";
+
+                        using (SQLiteCommand command = new SQLiteCommand(sql, conn))
+                        {
+                            command.Parameters.AddWithValue("@BucketName", bucketName);
+                            command.Parameters.AddWithValue("@Time", time);
+
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+
+            // Overwrite the CSV file with just the header
+            using (StreamWriter writer = new StreamWriter("TimeBuckets.csv"))
+            {
+                writer.WriteLine(header);
+            }
+
+            // Reload the time buckets into the ListBox
+            LoadTimeBuckets();
+        }
+
 
         private void unlockButton_Click(object sender, EventArgs e)
         {
@@ -161,6 +263,44 @@ namespace WinFormsActiveTango
             {
                 MessageBox.Show("Invalid input. Please enter a number.");
                 minutesBox.Clear();
+            }
+        }
+
+        private void UseButton_Click(object sender, EventArgs e)
+        {
+            if (timeBucketsListBox.SelectedItem != null)
+            {
+                string selectedItem = timeBucketsListBox.SelectedItem.ToString();
+                string bucketName = selectedItem.Substring(0, selectedItem.IndexOf("("));
+                string time = selectedItem.Substring(selectedItem.IndexOf("(") + 1, selectedItem.IndexOf(")") - selectedItem.IndexOf("(") - 1);
+
+                // Add the selected number of minutes to the countdown timer
+                MinutesUntilBlock_updated += int.Parse(time);
+                blockScreenForm.UpdateTimer(MinutesUntilBlock_updated);
+                timer.Stop();
+                timer.Start();
+
+                // Delete the selected item from the TimeBuckets table
+                using (SQLiteConnection conn = new SQLiteConnection("Data Source=tasks.db;Version=3;"))
+                {
+                    conn.Open();
+
+                    string sql = "DELETE FROM TimeBuckets WHERE BucketName = @BucketName AND Time = @Time";
+
+                    using (SQLiteCommand command = new SQLiteCommand(sql, conn))
+                    {
+                        command.Parameters.AddWithValue("@BucketName", bucketName);
+                        command.Parameters.AddWithValue("@Time", time);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                // Remove the selected item from the ListBox
+                timeBucketsListBox.Items.Remove(selectedItem);
+
+
+
             }
         }
 
